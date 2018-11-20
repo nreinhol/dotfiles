f1ac95d4cdf2807ae645bda7089ffb14645966a4
nreinhol <nreinhold24@gmail.com>
2018-11-20 20:57:39 +0100

    add custom.zsh for custom aliases

---
+++ custom.zsh
@@ -0,0 +1,9 @@
+# On Startup
+
+# Alias
+alias mount-drive='google-drive-ocamlfuse ~/googledrive'
+alias c='clear'
+alias clock='tty-clock -cSC4'
+alias vtop='vtop --theme wizard'
+alias weather='curl wttr.in/cologne'
+alias connectvm='ssh ewiis3@es.powertac.uni-koeln.de'
